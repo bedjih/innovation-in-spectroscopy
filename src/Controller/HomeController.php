745bfacf7762ce862ad6107a322b852d5d6bed0b
bedjih <bed.tassa@gmail.com>
2022-07-19 22:31:21 +0200

    test git branch paypal

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
-    {
+    {   // test branch paypal
         return $this->render('home/index.html.twig');
     }
 }
